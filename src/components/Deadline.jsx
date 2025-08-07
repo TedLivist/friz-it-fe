f9f819bc89785fbda77f287fa2d05ebb4cbcb9d0
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2025-08-07 13:55:10 +0100

    Sketch out deadline adjustment page

--- src/components/Deadline.jsx
+++ src/components/Deadline.jsx
@@ -1,11 +1,17 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Web3Context } from "../App";
+import { timeCalculation } from "../../utils/timeCalculation";
 
 const Deadline = () => {
 
   const {contract, signer, deadline} = useContext(Web3Context)
 
   const [newDeadline, setNewDeadline] = useState('')
+  const [timerQuote, setTimerQuote] = useState('')
+
+  useEffect(() => {
+    setTimerQuote(timeCalculation(Date.now(), (deadline * 1000)))
+  }, [deadline])
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -23,20 +29,28 @@ const Deadline = () => {
 
   return (
     <>
-      {console.log(signer.address)}
-      {console.log(deadline)}
-      Manage deadline
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="newDeadline">Set new deadline</label>
-        <input
-          name="newDeadline"
-          type="datetime-local"
-          value={newDeadline}
-          onChange={(e) => setNewDeadline(e.target.value)}
-          placeholder="Enter new deadline"
-        />
-        <button type="submit">Set Deadline</button>
-      </form>
+      {signer && (
+        <div>
+          <div>{timerQuote}</div>
+          <div>Manage deadline</div>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="newDeadline">Set new deadline</label>
+            <input
+              name="newDeadline"
+              type="datetime-local"
+              value={newDeadline}
+              onChange={(e) => setNewDeadline(e.target.value)}
+              placeholder="Enter new deadline"
+            />
+            <button type="submit">Set Deadline</button>
+          </form>
+          <div>
+            Only the owner can adjust the deadline <br/>
+            And the newer deadline must be later than the existing deadline <br/>
+            Gas fees apply for this action
+          </div>
+        </div>
+      )}
     </>
   );
 }
